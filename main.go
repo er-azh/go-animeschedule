12233ef35f4a1c4926d2e54f1709af5f978d8236
er-azh <80633916+er-azh@users.noreply.github.com>
2022-05-09 13:08:54 +0430

    format code

--- main.go
+++ main.go
@@ -34,30 +34,29 @@ func getJSON[T any](url string) (*T, error) {
 	}
 
 	var resp T
-	json.NewDecoder(response.Body).Decode(&resp)
-	return &resp, nil
+	return &resp, json.NewDecoder(response.Body).Decode(&resp)
 }
 
 type TimetableTime struct {
-    Year, Week int
+	Year, Week int
 }
 
 // GetCurrentTimetable gets the current timetable or the timetable in a specific week
 // when timetableTime is specified it tries to get that week's timetable.
 // timezone can be set to return times in a different time zone.
-func GetCurrentTimetable(timetableTime *TimetableTime,timezone *time.Location) (*Timetable, error) {
-    values := url.Values{}
-    if timetableTime != nil {
-        values.Add("year", strconv.Itoa(timetableTime.Year))
-        values.Add("week", strconv.Itoa(timetableTime.Week))
-    }
-    if timezone != nil {
-        values.Add("tz", timezone.String())
-    }
-	return getJSON[Timetable](APIRoot+"/timetables?" + values.Encode())
+func GetCurrentTimetable(timetableTime *TimetableTime, timezone *time.Location) (*Timetable, error) {
+	values := url.Values{}
+	if timetableTime != nil {
+		values.Add("year", strconv.Itoa(timetableTime.Year))
+		values.Add("week", strconv.Itoa(timetableTime.Week))
+	}
+	if timezone != nil {
+		values.Add("tz", timezone.String())
+	}
+	return getJSON[Timetable](APIRoot + "/timetables?" + values.Encode())
 }
 
 // GetShow gets a show by its slug
 func GetShow(slug string) (*ShowDetail, error) {
-	return getJSON[ShowDetail](APIRoot+"/anime/"+url.QueryEscape(slug))
+	return getJSON[ShowDetail](APIRoot + "/anime/" + url.QueryEscape(slug))
 }
